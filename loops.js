f8cf5327643071a9615850eecadd571e6d9dbbb6
Armando <salixonline@gmail.com>
2017-09-19 04:57:44 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,9 @@
+function forLoop(array) {
+  for (var i = 0; i < array.length; i++) {
+    if (i === 1) {
+      console.log(`I am 1 strange loop`);
+    } else {
+      console.log(`I am ${i} strange loops`);
+    }
+  }
+}
